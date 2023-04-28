7e153ac678d91e8d6778a3ad3d66f9d2ae518ede
Striver <131383897+StriverStrikes@users.noreply.github.com>
2023-04-28 13:47:13 +0530

    Update puppet.js

--- puppet.js
+++ puppet.js
@@ -14,29 +14,11 @@ async function script() {
 
     const page = await browser.newPage();
 
-    await page.goto('https://developer.chrome.com/');
-
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
-
-    // Type into search box
-    await page.type('.search-box__input', 'automate beyond recorder');
-
-    // Wait and click on first result
-    const searchResultSelector = '.search-box__link';
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-        'text/Customize and automate'
-    );
-    const fullTitle = await textSelector.evaluate(el => el.textContent);
-
-    // Print the full title
-    console.log('The title of this blog post is "%s".', fullTitle);
-
-    await browser.close();
+    await page.goto('https://google.com');
+    console.log('page opened successfully');
+    await page.screenshot({ path: __dirname + '/screenshot.png' });
+    console.log('screen shot captured');
+    browser.close();
 }
 
-script()
+script();
